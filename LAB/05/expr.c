553c78d9012e2919b194b8a30bea773da6cc8bc3
Arshdeep Singh <singh.arshdeep1999@gmail.com>
2018-02-18 14:05:27 +0530

    Create expr.c

---
+++ LAB/05/expr.c
@@ -0,0 +1,11 @@
+//checking value of expression
+//please calculate it yourself before runing it
+#include <stdio.h>
+
+int main(){
+	double abc = 2.5;
+	int big = 2;
+	double g = big/2 + big*4/big - big + abc/3;
+	printf("%llf",g);
+	return 0;
+}
